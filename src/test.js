119fcb4b640e59de47f9c15b9c456ba9dfab3624
marwan211 <samroutc@gmail.com>
2023-03-21 13:01:58 +0200

    upload6

--- src/test.js
+++ src/test.js
@@ -1,109 +1,81 @@
 const express = require('express');
-const multer = require('multer');
+const app = express();
 const path = require('path');
 const fs = require('fs');
-const app = express();
+const multer = require('multer');
+
 const folderPath = path.join(__dirname, 'files');
-// Set up multer storage configuration
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'files/');
-  },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const date = new Date().toISOString().replace(/:/g, '-');
-    cb(null, `${date}-${file.originalname}`);
-  }
+
+// set up multer for file uploads
+const upload = multer({ dest: folderPath });
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
 });
 
-// Set up multer upload configuration
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5 // 5MB
-  },
-  fileFilter: function (req, file, cb) {
-    const filetypes = /jpeg|jpg|png|gif/;
-    const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-    if (mimetype && extname) {
-      return cb(null, true);
-    }
-    cb("Error: File upload only supports the following filetypes - " + filetypes);
-  }
-}).single('image'); // The name of the file input field on the form
-
-app.post('/upload', function (req, res) {
-  upload(req, res, function (err) {
+app.get('/file', function (req, res) {
+  fs.mkdir(folderPath, (err) => {
     if (err) {
-      res.status(400).send(err);
+      console.error(err);
+      res.send('error');
     } else {
-      if (!req.file) {
-        res.status(400).send("No file selected for upload");
-      } else {
-        res.send({
-          message: "File uploaded successfully",
-          filename: req.file.filename
-        });
-      }
-    }
-  });
-});
-
-app.get('/view/:filename', function(req, res) {
-    const filename = req.params.filename;
-    const imagePath = path.join(__dirname, 'files', filename);
-  
-    // Check if file exists
-    if (fs.existsSync(imagePath)) {
-      // Set content type based on file extension
-      const extname = path.extname(imagePath);
-      let contentType;
-      switch (extname) {
-        case '.jpg':
-          contentType = 'image/jpeg';
-          break;
-        case '.png':
-          contentType = 'image/png';
-          break;
-        case '.gif':
-          contentType = 'image/gif';
-          break;
-        default:
-          contentType = 'application/octet-stream';
-      }
-  
-      // Read file and send as response
-      fs.readFile(imagePath, function(err, data) {
+      console.log('Folder created successfully');
+      const filePath = path.join(folderPath, 'example.txt');
+      console.log(filePath);
+      fs.writeFile(filePath, 'This is an example text file.', (err) => {
         if (err) {
-          res.status(500).send(err);
+          console.error(err);
+          res.send('error');
         } else {
-          res.set('Content-Type', contentType);
-          res.send(data);
+          console.log('File created successfully');
+          res.send('success');
         }
       });
-    } else {
-      res.status(404).send('File not found');
     }
   });
-  app.get('/view', function(req, res) {
-    const directoryPath = folderPath
-    fs.readdir(directoryPath, function(err, files) {
-      if (err) {
-        return console.log('Unable to scan directory: ' + err);
+});
+
+// upload endpoint for images
+app.post('/upload', upload.single('image'), function (req, res) {
+  const imagePath = req.file.path;
+  console.log('File uploaded successfully');
+
+  res.send('success');
+});
+
+// view endpoint for images
+app.get('/view', function (req, res) {
+  fs.readdir(folderPath, function (err, files) {
+    if (err) {
+      console.error(err);
+      res.send('error');
+    } else {
+      const images = files.filter((file) =>
+        ['.jpg', '.jpeg', '.png'].includes(path.extname(file))
+      );
+      const html = images
+        .map((image) => `<img src="/files/${image}" />`)
+        .join('');
+
+      res.send(html);
+    }
+  });
+});
+app.get('/views', function(req, res) {
+  const directoryPath = folderPath
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    const fileNames = files.map(file => {
+      return {
+        name: file,
+        url: `http://${req.headers.host}/filessss/${file}`
       }
-      const fileNames = files.map(file => {
-        return {
-          name: file,
-          url: `http://${req.headers.host}/filessss/${file}`
-        }
-      })
-      res.send(fileNames);
-    });
+    })
+    res.send(fileNames);
   });
-  // ...
-  
-  app.listen(3000, function () {
-    console.log('Server started on port 3000');
-  });
+});
+
+
+app.listen(3000);
