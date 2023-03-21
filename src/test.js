1a0a81fc8373bcbbb0dd21e23f91a62a64dd7d80
marwan211 <samroutc@gmail.com>
2023-03-21 12:15:51 +0200

    upload1

--- src/test.js
+++ src/test.js
@@ -1,47 +1,109 @@
 const express = require('express');
-const app = express();
+const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const app = express();
 
-const folderPath = path.join(__dirname, 'files');
-
-app.get('/', function (req, res) {
-  res.send('Hello World');
+// Set up multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'upload/');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const date = new Date().toISOString().replace(/:/g, '-');
+    cb(null, `${date}-${file.originalname}`);
+  }
 });
 
-app.get('/file', function (req, res) {
-  fs.mkdir(folderPath, (err) => {
+// Set up multer upload configuration
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5 // 5MB
+  },
+  fileFilter: function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb("Error: File upload only supports the following filetypes - " + filetypes);
+  }
+}).single('image'); // The name of the file input field on the form
+
+app.post('/upload', function (req, res) {
+  upload(req, res, function (err) {
     if (err) {
-      console.error(err);
-      res.send('error');
+      res.status(400).send(err);
     } else {
-      console.log('Folder created successfully');
-      const filePath = path.join(folderPath, 'example.txt');
-      console.log(filePath);
-      fs.writeFile(filePath, 'This is an example text file.', (err) => {
+      if (!req.file) {
+        res.status(400).send("No file selected for upload");
+      } else {
+        res.send({
+          message: "File uploaded successfully",
+          filename: req.file.filename
+        });
+      }
+    }
+  });
+});
+
+app.get('/view/:filename', function(req, res) {
+    const filename = req.params.filename;
+    const imagePath = path.join(__dirname, 'upload', filename);
+  
+    // Check if file exists
+    if (fs.existsSync(imagePath)) {
+      // Set content type based on file extension
+      const extname = path.extname(imagePath);
+      let contentType;
+      switch (extname) {
+        case '.jpg':
+          contentType = 'image/jpeg';
+          break;
+        case '.png':
+          contentType = 'image/png';
+          break;
+        case '.gif':
+          contentType = 'image/gif';
+          break;
+        default:
+          contentType = 'application/octet-stream';
+      }
+  
+      // Read file and send as response
+      fs.readFile(imagePath, function(err, data) {
         if (err) {
-          console.error(err);
-          res.send('error');
+          res.status(500).send(err);
         } else {
-          console.log('File created successfully');
-          res.send('success');
+          res.set('Content-Type', contentType);
+          res.send(data);
         }
       });
-    }
-  });
-});
-
-app.get('/read', function (req, res) {
-  const filePath2 = path.join(folderPath, 'example.txt');
-  fs.readFile(filePath2, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.send('error');
     } else {
-      console.log('File read successfully');
-      res.send(data);
+      res.status(404).send('File not found');
     }
   });
-});
-
-app.listen(3000);
+  app.get('/view', function(req, res) {
+    const directoryPath = path.join(__dirname, 'upload');
+    fs.readdir(directoryPath, function(err, files) {
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+      const fileNames = files.map(file => {
+        return {
+          name: file,
+          url: `http://${req.headers.host}/upload/${file}`
+        }
+      })
+      res.send(fileNames);
+    });
+  });
+  // ...
+  
+  app.listen(3000, function () {
+    console.log('Server started on port 3000');
+  });
